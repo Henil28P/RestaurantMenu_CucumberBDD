9dc5930832061f2b14c9fc17ca166e86f1deb88f
Henil28P <henilp467@gmail.com>
2025-01-07 20:17:33 +1100

    add all initial setup files of Cucumber BDD Project using Java and Maven Quickstart archetype

---
+++ src/main/java/restaurantmenu/cucumberbddproject/App.java
@@ -0,0 +1,13 @@
+package restaurantmenu.cucumberbddproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
