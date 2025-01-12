8533dbd0f202023542ee929fb35939741125088a
Henil28P <henilp467@gmail.com>
2025-01-12 13:57:29 +1100

    [MenuManagementSteps.java]: finish implementation of 1st step of @Given method and make NewMenuItem a class field/attribute

--- src/test/java/stepdefinitions/MenuManagementSteps.java
+++ src/test/java/stepdefinitions/MenuManagementSteps.java
@@ -9,12 +9,14 @@ public class MenuManagementSteps {
 	// Name of method doesn't matter
 	// Each method has an annotation that contains text that matches text specified in the feature file scenario steps
 	// Some values in "" in feature file scenario steps (such as "Cucumber sandwich" and 20) are extracted as method parameters (string, int) which are Cucumber expressions which can be available to be used in the method
-
+	
+    // Create a restaurant menu item - and this will need us to create a class called RestaurantMenuItem Java class as it is not defined yet in src/main/java/restaurantmenu.cucumberbddproject directory of this project which will stay along with App.java class
+	RestaurantMenuItem NewMenuItem; // make it a class field/attribute
+	
 	@Given("I have a menu item with name {string} and price {int}")
 	public void i_have_a_menu_item_with_name_and_price(String newMenuItemName, Integer price) {
-	    // Create a restaurant menu item - and this will need us to create a class called RestaurantMenuItem Java class as it is not defined yet in src/main/java/restaurantmenu.cucumberbddproject directory of this project which will stay along with App.java class
-		RestaurantMenuItem NewMenuItem;
-		NewMenuItem = new RestaurantMenuItem(newMenuItemName, "", price); // the RestaurantMenuItem class will take 3 parameters (item name, description, price) to give to constructor
+		// instantiate the RestaurantMenuItem for NewMenuItem field
+		NewMenuItem = new RestaurantMenuItem(newMenuItemName, newMenuItemName, price); // the RestaurantMenuItem class will take 3 parameters (item name, description, price) to give to constructor
 	}
 
 	@When("I add that menu item")
