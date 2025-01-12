120d4f65d0ab8a99226bebda4673adb933f545fb
Henil28P <henilp467@gmail.com>
2025-01-12 14:27:36 +1100

    [RestaurantMenu.java]: create a new MenuItems arraylist and define a new method of addMenuItem() to add new menu item (newMenuItem field) to the arraylist

--- src/main/java/restaurantmenu/cucumberbddproject/RestaurantMenu.java
+++ src/main/java/restaurantmenu/cucumberbddproject/RestaurantMenu.java
@@ -1,5 +1,16 @@
 package restaurantmenu.cucumberbddproject;
 
+import java.util.ArrayList;
+
+// This class is used in @When method of step definitions file
 public class RestaurantMenu {
 
+	// Below ArrayList holds an array list of restaurant menu items
+	ArrayList<RestaurantMenuItem> MenuItems = new ArrayList<RestaurantMenuItems>();
+
+	// To add a menu item to the above MenuItems array list
+	public boolean addMenuItem(RestaurantMenuItem newMenuItem) throws IllegalArgumentException
+	{
+		return MenuItems.add(newMenuItem);
+	}
 }
