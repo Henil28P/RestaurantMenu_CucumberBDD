d56ef6459a004d6d7889532a5aedd231519c19f9
Henil28P <henilp467@gmail.com>
2025-01-12 12:59:04 +1100

    [RestaurantMenuItem.java]: add 3 fields, constructor, their getters and setters for creating a new menu item name

--- src/main/java/restaurantmenu/cucumberbddproject/RestaurantMenuItem.java
+++ src/main/java/restaurantmenu/cucumberbddproject/RestaurantMenuItem.java
@@ -1,5 +1,39 @@
 package restaurantmenu.cucumberbddproject;
 
+// Implementation is driver by step definitions, which is driver by our Feature file which ensures Test-first development
 public class RestaurantMenuItem {
+	// Add 3 attributes/fields to this class
+	private String ItemName;
+	private String Description;
+	private int price;
 
+	// Create a constructor for RestaurantMenuItem class
+	public RestaurantMenuItem(String itemName, String description, int price) {
+		super();
+		ItemName = itemName;
+		Description = description;
+		this.price = price;
+	}
+
+	// Create getters and setters for each of the above 3 attributes
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getItemName() {
+		return ItemName;
+	}
+	// Note: No setter for ItemName attribute because once an item is created with ItemNam, it cannot be changed later
 }
