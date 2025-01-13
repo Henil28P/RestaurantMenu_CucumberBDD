24c98d004551fdd09d4654462b36c9488595b466
Henil28P <henilp467@gmail.com>
2025-01-13 14:30:53 +1100

    [MenuManagementTest.java]: define @RunWith annotation and import a Cucumber API JUnit

--- src/test/java/testrunners/MenuManagementTest.java
+++ src/test/java/testrunners/MenuManagementTest.java
@@ -1,6 +1,10 @@
 package testrunners;
 
+import org.junit.runner.RunWith;
+import cucumber.api.junit.Cucumber;
+
 // Test Runner class file to run our Cucumber test after all Step definitions methods are implemented
+@RunWith(Cucumber.class) // define @RunWith annotation and it will import a Cucumber API JUnit
 public class MenuManagementTest {
 
 }
