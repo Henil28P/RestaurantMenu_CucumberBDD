f42c5931889d9e666e116e9ab0805287ed619bdd
Henil28P <henilp467@gmail.com>
2025-01-13 14:46:34 +1100

    [MenuManagementTest.java]: add a plugin parameter to @CucumberOptions class definition for generating nice-looking test outputs

--- src/test/java/testrunners/MenuManagementTest.java
+++ src/test/java/testrunners/MenuManagementTest.java
@@ -8,13 +8,14 @@ import cucumber.api.junit.Cucumber;
 // Test Runner class file to run our Cucumber test after all Step definitions methods are implemented
 @RunWith(Cucumber.class) // define @RunWith annotation and it will import a Cucumber API JUnit
 // define a few Cucumber options
-//this class will link the Feature file with the step definitions
+// this class will link the Feature file with the step definitions
 @CucumberOptions(
-		features="src/test/java/restaurantmenu/cucumberbddproject/features", // path of where all features are located (in the "features" package)
-		glue="stepdefinitions" // package name of where the step definitions are located
+		features={"src/test/java/restaurantmenu/cucumberbddproject/features"}, // path of where all features are located (in the "features" package)
+		glue={"stepdefinitions"}, // package name of where the step definitions are located
+		plugin={"pretty"} // use a plugin "pretty" which will be used when generating nice looking outputs
 		)
-// The above 2 parameters of "features" and "glue"
 // No implementation in this class as it tells the build tool or Java compiler to use Cucumber tests (CucumberOptions) and it links the feature file with the glue (stepdefinitions)
+// Note: All the parameter values of @CucumberOptions are multiple values or an array of strings (eg. for glue parameter, we can specify multiple package names as step definitions could be in multiple files sometimes)
 // Save this test runner file and run it as a JUnit test
 public class MenuManagementTest {
 
