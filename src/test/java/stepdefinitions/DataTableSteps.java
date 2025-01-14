9e093b4920a89e06b239274dca791c4c5e7a9df3
Henil28P <henilp467@gmail.com>
2025-01-14 14:59:17 +1100

    [DataTableSteps.java]: define and import methods for @Given, @When, @Then annotations (@Given method is the focus in data tables)

--- src/test/java/stepdefinitions/DataTableSteps.java
+++ src/test/java/stepdefinitions/DataTableSteps.java
@@ -1,5 +1,29 @@
 package stepdefinitions;
 
-public class DataTableSteps {
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
 
+public class DataTableSteps {
+	@Given("I placed an order for the following items")
+	public void i_placed_an_order_for_the_following_items(io.cucumber.datatable.DataTable dataTable) {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+	    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+	    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+	    //
+	    // For other transformations you can register a DataTableType.
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("I generate the bill")
+	public void i_generate_the_bill() {
+
+	}
+
+	@Then("a bill for ${int} should be generated")
+	public void a_bill_for_$_should_be_generated(Integer int1) {
+
+	}
 }
