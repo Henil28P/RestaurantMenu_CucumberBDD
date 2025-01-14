812f1ea7a616650fac3cbbed0e0e9c10e41b24d7
Henil28P <henilp467@gmail.com>
2025-01-14 13:45:42 +1100

    [RestaurantSystemHooks.java]: create 2 methods for annotations (1 for before and 1 for after) and import both with cucumber.api.java

--- src/test/java/hooks/RestaurantSystemHooks.java
+++ src/test/java/hooks/RestaurantSystemHooks.java
@@ -1,5 +1,18 @@
 package hooks;
 
-public class RestaurantSystemHooks {
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 
+public class RestaurantSystemHooks {
+	@Before
+	public void BeforeDisplayMessage()
+	{
+		System.out.println("Before");
+	}
+
+	@After
+	public void AfterDisplayMessage()
+	{
+		System.out.println("After");
+	}
 }
