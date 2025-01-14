77bd350403b48bd97743d6b2c49e795571291d11
Henil28P <henilp467@gmail.com>
2025-01-14 14:39:29 +1100

    [MenuManagementTest.java]: add 'monochrome' parameter and set to true to make outputs more readable and remove any unnecessary special characters

--- src/test/java/testrunners/MenuManagementTest.java
+++ src/test/java/testrunners/MenuManagementTest.java
@@ -17,7 +17,8 @@ import cucumber.api.junit.Cucumber;
 				"html:target/SystemTestReports/html",
 				"json:target/SystemTestReports/json/report.json",
 				"junit:target/SystemTestReports/junit/report.xml"}, // use a plugin "pretty" which will be used when generating nice looking outputs
-		dryRun=false // default value of dryRun parameter is false
+		dryRun=false, // default value of dryRun parameter is false
+		monochrome=true // for more readable outputs and removes all special characters
 		)
 // No implementation in this class as it tells the build tool or Java compiler to use Cucumber tests (CucumberOptions) and it links the feature file with the glue (stepdefinitions)
 // Note: All the parameter values of @CucumberOptions are multiple values or an array of strings (eg. for glue parameter, we can specify multiple package names as step definitions could be in multiple files sometimes)
