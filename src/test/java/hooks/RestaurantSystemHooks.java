e4b62b5d5afc7c54959073e5fbacef59649518e7
Henil28P <henilp467@gmail.com>
2025-01-14 13:52:40 +1100

    [RestaurantSystemHooks.java]: import and use cucumber.api.Scenario to print name of scenario when called both Before and After methods

--- src/test/java/hooks/RestaurantSystemHooks.java
+++ src/test/java/hooks/RestaurantSystemHooks.java
@@ -1,18 +1,19 @@
 package hooks;
 
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
 public class RestaurantSystemHooks {
 	@Before
-	public void BeforeDisplayMessage()
+	public void BeforeDisplayMessage(Scenario sc)
 	{
-		System.out.println("Before");
+		System.out.println("Before " + sc.getName());
 	}
 
 	@After
-	public void AfterDisplayMessage()
+	public void AfterDisplayMessage(Scenario sc)
 	{
-		System.out.println("After");
+		System.out.println("After " + sc.getName());
 	}
 }
